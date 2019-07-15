58fc0d5bdd886c5772bef522de15b9ba5a59054a
angelgus79 <agustavo79@gmail.com>
2019-07-14 17:05:56 -0700

    Example controler was added

---
+++ Practice08/Controllers/ExampleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice08.Services;
+
+
+namespace Practice08.Models
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+
+        ServiceProduct Products;
+        public ExampleController()
+        {
+            Products = new ServiceProduct();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*  Learning view Action*/
+        public ViewResult GetProduct(int ProductId)
+        {
+
+            var product = (from p in Products.Products
+                          where p.ProductId == ProductId
+                          select p).FirstOrDefault();
+            return View(product);
+        }
+    }
+}
