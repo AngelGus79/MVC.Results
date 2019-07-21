4794e8f8fec655a6f63fcf2efc7add51f01a39c7
angelgus79 <agustavo79@gmail.com>
2019-07-21 11:38:41 -0700

    scaffolding of the product class

--- Practice08/Models/Product.cs
+++ Practice08/Models/Product.cs
@@ -11,6 +11,9 @@ namespace Practice08.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public bool InStock { get; set; }
+        public string Category { get; set; }
+
+        public DateTime FirstDate { get; set; }
 
     }
 }
